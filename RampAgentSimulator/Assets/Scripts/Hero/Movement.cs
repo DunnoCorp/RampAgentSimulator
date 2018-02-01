a339018ea9073ef47ebb16b88113703bfeac6cb9
Thomas Lebrun <thomas.lbrn76@gmail.com>
2018-02-01 15:28:58 +0100

    IK working

---
+++ RampAgentSimulator/Assets/Scripts/Hero/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Movement {
+
+    public Vector3 m_LeftHandPos;
+    public Vector3 m_RightHandPos;
+    public Vector3 m_LeftHandRot;
+    public Vector3 m_RightHandRot;
+
+    public KeyCode m_Key;
+
+}
