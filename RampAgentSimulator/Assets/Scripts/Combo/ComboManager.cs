29c2b1b6aca4baab5d96ffd7cce3989dc92393bd
joubierjc <joubier.j.c@gmail.com>
2018-02-01 15:19:54 +0100

    ComboManager is working

---
+++ RampAgentSimulator/Assets/Scripts/Combo/ComboManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboManager : MonoBehaviour {
+    public static ComboManager current;
+
+    public KeyCode[] allowedKeys;
+
+    public string display;
+
+    public Combo[] combos;
+    public List<KeyCode> currentCombo;
+
+    public void Awake() {
+        current = this;
+        currentCombo = new List<KeyCode>();
+    }
+
+    private void Update() {
+        foreach (var key in allowedKeys) {
+            if(Input.GetKeyDown(key)) {
+                currentCombo.Add(key);
+                break;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
+            display = "NOPE";
+            foreach (var item in combos) {
+                if (item.Check(currentCombo)) {
+                    display = item.onFinished;
+                    break;
+                }
+            }
+            Debug.Log(display);
+            currentCombo.Clear();
+        }
+    }
+}
